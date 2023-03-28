c38631129bd92a7e4102bd268d2f3e1dc4e3035b
Tinu_thampi <79778565+Tinu-thampi13@users.noreply.github.com>
2023-03-28 17:40:11 +0100

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,209 @@
+// ignore_for_file: avoid_unnecessary_containers, camel_case_types, prefer_interpolation_to_compose_strings, curly_braces_in_flow_control_structures, prefer_const_constructors_in_immutables, prefer_const_constructors
+
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(myApp());
+
+class myApp extends StatelessWidget {
+  const myApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: calculator(),
+    );
+  }
+}
+
+class calculator extends StatefulWidget {
+  calculator({super.key});
+
+  @override
+  State<calculator> createState() => _calculatorState();
+}
+
+class _calculatorState extends State<calculator> {
+  String numtext = "";
+  String rand = "";
+  int count = 0;
+  String dob = "";
+
+  String random() {
+    var rng = Random();
+    for (var i = 0; i < 10; i++) {
+      rand = numtext + (rng.nextInt(1000)).toString();
+    }
+    return rand;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Calculator',
+          style: TextStyle(
+            fontSize: 25,
+          ),
+        ),
+        backgroundColor: Colors.black,
+      ),
+      backgroundColor: Colors.black,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 5),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            SizedBox(
+              height: 300,
+              width: 380,
+              child: ListView(
+                  // scrollDirection: Axis.horizontal,
+                  reverse: true,
+                  physics: const BouncingScrollPhysics(),
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.all(15.0),
+                      alignment: Alignment.centerRight,
+                      child: Text(
+                        numtext,
+                        textAlign: TextAlign.end,
+                        style: TextStyle(fontSize: 40, color: Colors.white),
+                      ),
+                    ),
+                  ]),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 28.0, right: 28),
+              child: Divider(
+                color: Color.fromARGB(255, 106, 103, 103),
+                thickness: 1,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                ),
+                // const SizedBox(
+                //   height: 5,
+                // ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        blocks("AC", Colors.red),
+                        blocks("C", Colors.red),
+                        blocks("%", Colors.green),
+                        blocks("/", Colors.green)
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        blocks("1"),
+                        blocks("2"),
+                        blocks("3"),
+                        blocks("*", Colors.green)
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        blocks("4"),
+                        blocks("5"),
+                        blocks("6"),
+                        blocks("-", Colors.green)
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        blocks("7"),
+                        blocks("8"),
+                        blocks("9"),
+                        blocks("+", Colors.green)
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        blocks("."),
+                        blocks("0"),
+                        blocks("00"),
+                        blocks("=", Colors.amber)
+                      ],
+                    )
+                  ],
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 25,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget blocks(String text, [Color? color]) {
+    return GestureDetector(
+      onTap: () {
+        if (text == "AC") {
+          setState(() {
+            numtext = "";
+            count = 0;
+          });
+        } else if (text == "*") {
+          if (count == 0) {
+            dob = numtext;
+            count += 1;
+            numtext = numtext + "*";
+          }
+        } else if (text == ".") {
+          setState(() {
+            numtext = "";
+            numtext = dob;
+          });
+          print(dob);
+        } else if (text == "=") {
+          setState(() {
+            numtext = '';
+            numtext = random();
+          });
+        } else {
+          setState(() {
+            numtext = numtext + text;
+          });
+        }
+      },
+      child: Padding(
+        padding: const EdgeInsets.only(top: 10, left: 8, right: 8),
+        child: Container(
+          height: 70,
+          width: 70,
+          decoration: BoxDecoration(
+              color: Colors.grey[800], borderRadius: BorderRadius.circular(10)),
+          child: Center(
+              child: Text(
+            text,
+            style: TextStyle(
+                fontSize: 25,
+                color: color ?? Colors.white,
+                fontWeight: FontWeight.bold),
+          )),
+        ),
+      ),
+    );
+  }
+}
